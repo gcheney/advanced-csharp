588f339a3cea1ef26729d511f84caeec8e4acfa1
Glendon Cheney <glendon.cheney@gmail.com>
2016-06-30 12:44:42 -0500

    Finish extension methods section

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ExtenssionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is very long blah blah blah...";
+            var shortednPost = post.Shorten(5);
+
+            IEnumerable<int> numbers = new List<int>() { 1, 3, 5, 6, 7, 8 };
+            var max = numbers.Max();
+
+            Console.WriteLine(shortednPost + " " + max);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+
+namespace System //same namespace as String class
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numOfWords)
+        {
+            if (numOfWords < 0)
+            {
+                throw new ArgumentOutOfRangeException("Number of words must "
+                    + "be greater or equal to 0");
+            }
+
+            if (numOfWords == 0)
+            {
+                return string.Empty;
+            }
+
+            var words = str.Split(' ');
+
+            if (words.Length <= numOfWords)
+            {
+                return str;
+            }
+
+            var newStr = string.Join(" ", words.Take(numOfWords)) + "....";
+            return newStr;
+        }
+    }
+}
