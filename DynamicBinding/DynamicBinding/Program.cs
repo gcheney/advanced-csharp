6821238eb9104bdb99de34ef38155a3da6d20825
Glendon Cheney <glendon.cheney@gmail.com>
2016-07-01 19:32:27 -0500

    Complete dynamic binding section

---
+++ DynamicBinding/DynamicBinding/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DynamicBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic excelObject = "Mosh";
+            excelObject.Optimize();
+
+            dynamic name = "Mosh";
+            name = 10;
+            Console.WriteLine("Name: " + name);
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b; //becomes dynamic
+            Console.WriteLine(c);
+
+            int i = 5;
+            dynamic d = i; //Becomes int at run-time
+        }
+    }
+}
