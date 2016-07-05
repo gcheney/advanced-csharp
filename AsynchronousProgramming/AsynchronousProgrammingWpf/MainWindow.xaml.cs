453d5fd71a7182f1b84064599ef2629e057a0b92
Glendon Cheney <glendon.cheney@gmail.com>
2016-07-05 12:55:06 -0500

    Complete async programming section

---
+++ AsynchronousProgramming/AsynchronousProgrammingWpf/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsynchronousProgrammingWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            var getHtmlTask = GetHtmlAsync("http://msdn.microsoft.com");
+            MessageBox.Show("Waiting for operation");
+            //await can be delayed
+            var html = await getHtmlTask;
+            MessageBox.Show(html.Substring(0, 10));
+        }
+
+        public async Task<string> GetHtmlAsync(string url)
+        {
+            var webClient = new WebClient();
+            return await webClient.DownloadStringTaskAsync(url); //imagine it takes a while
+        }
+
+        public string GetHtml(string url)
+        {
+            var webClient = new WebClient();
+            return webClient.DownloadString(url); //imagine it takes awhile
+        }
+    }
+}
