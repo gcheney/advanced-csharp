4d5b889b9e6a2dc9be6277bdfc23f5f4e3fc4403
Glendon Cheney <glendon.cheney@gmail.com>
2016-07-09 13:14:21 -0500

    Complete Nullable Types

---
+++ NullableTypes/NullableTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime? date = null;
+
+            Console.WriteLine("GetValueOrDefault() " + date.GetValueOrDefault());
+            Console.WriteLine("HasValue: " + date.HasValue);
+            //Console.WriteLine("Value: " + date.Value); //Exception!
+
+            DateTime? date2 = new DateTime(2914, 2, 2);
+            DateTime date3 = date2.GetValueOrDefault();
+            DateTime? date4 = date3;
+            Console.WriteLine(date4.GetValueOrDefault());
+
+
+            DateTime? time = null;
+            DateTime today;
+
+
+            //Long way
+            if (time != null)
+            {
+                today = time.GetValueOrDefault();
+            }
+            else
+            {
+                today = DateTime.Today;
+            }
+
+            DateTime today2 = (date != null) ? time.GetValueOrDefault() : DateTime.Today;
+            //null coalescing operator
+            today = time ?? DateTime.Today;
+
+            Console.WriteLine(today);
+        }
+    }
+}
